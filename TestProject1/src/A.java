02604a55b398a1a5e82d474e95cb4e475c430baf
chandandixit888 <56721890+chandandixit888@users.noreply.github.com>
2019-10-20 17:25:53 +0530

    Update A.java

--- TestProject1/src/A.java
+++ TestProject1/src/A.java
@@ -3,5 +3,6 @@ public class A
 public static void main(String[]args)
 {
 System.out.println("How to get new files available in GitHub to local system");
+System.out.println("How to get new files available in GitHub to local system");
 }
 }
