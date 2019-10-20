b1f0def0aab8a4ca5db6d6c3ef15032743d190ea
KD <KD@KD-PC>
2019-10-20 14:55:26 +0530

    Pushing  file

--- TestProject1/src/A.java
+++ TestProject1/src/A.java
@@ -1,6 +1,6 @@
 public class A
 {
-public static void main(string[]args)
+public static void main(String[]args)
 {
 System.out.println("How to get new files available in GitHub to local system");
 }
